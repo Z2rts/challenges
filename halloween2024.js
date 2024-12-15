f594ac4535a852cd46406d97315ef9d604036481
Zrts <Dracduf@tutanota.com>
2024-12-15 09:33:13 +0100

    Update halloween2024.js

--- halloween2024.js
+++ halloween2024.js
@@ -4,28 +4,6 @@
 
 // Challenge03
 
-/* // Challenge04
-const whisper = 'd~~~~~a';
-const suspects = ['Dracula', 'Freddy Krueger', 'Jason Voorhees', 'Michael Myers'];
-
-function findTheKiller(whisper, suspects) {
-  const regexString = '^' + whisper.replace(/~/g, '.') + '$'; // Reemplaza ~ por .
-  const regex = new RegExp(regexString, 'i'); // i: ignora mayúsculas/minúsculas
-
-  return suspects.map(suspect => {
-    const coincidencias = suspect.split('').reduce((count, char, i) => {
-      return whisper[i] && whisper[i].toLowerCase() === char.toLowerCase()
-        ? count + 1
-        : count;
-    }, 0);
-
-    const esCoincidencia = regex.test(suspect);
-
-    return { nombre: suspect, coincidencias, esCoincidencia };
-  })
-} 
-
-console.log(findTheKiller(whisper, suspects))
-*/
+// Challenge04
 
 // Challenge05
